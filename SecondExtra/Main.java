e7ccdab46c80b98022d7edfcbe7d36bdb6cc5347
Якунин Сергей <urugvai2105@gmal.com>
2021-01-18 22:42:46 +0500

    Задание №2

---
+++ SecondExtra/Main.java
@@ -0,0 +1,64 @@
+package SecondExtra;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Get user input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите данные: ");
+        String line = sc.nextLine().trim();
+
+        // Transforming user input to array of Integers
+        ArrayList<Integer> data = new ArrayList<>();
+        String[] strings = line.split(" ");
+        for (String string :
+                strings) {
+            data.add(Integer.valueOf(string));
+        }
+
+        int answer = getCoolInteger(new HashSet<>(data));
+        System.out.println(answer);
+    }
+
+    /**
+     * Возвращает наименьшее неотрицательное число
+     * которое можно: прибавить, вычесть или оставить без изменения элемент входной последовательности,
+     * чтобы все элементы последовательности стали равными
+     */
+    static public int getCoolInteger(Set<Integer> data) {
+        // By default the answer is -1
+        int result = -1;
+        int max = data
+                .stream()
+                .mapToInt(v -> v)
+                .max().orElseThrow(NoSuchElementException::new);
+        int min = data
+                .stream()
+                .mapToInt(v -> v)
+                .min().orElseThrow(NoSuchElementException::new);
+        int delta = max - min;
+
+        // If the difference between min and max is 0 or 1, then is answer is that difference
+        if (delta == 0 || delta == 1)
+            result = delta;
+
+        // Otherwise the answer could be half the difference, but we have to check every number
+        // in the array if it fits every criteria
+        else {
+            if (delta % 2 == 0) {
+                int possibleResult = delta / 2;
+                int requiredInt = min + possibleResult;
+                for (int item : data) {
+                    if ((item < requiredInt && item + possibleResult != requiredInt) ||
+                            (item > requiredInt && item - possibleResult != requiredInt)) {
+                        possibleResult = -1;
+                        break;
+                    }
+                }
+                result = possibleResult;
+            }
+        }
+        return result;
+    }
+}
